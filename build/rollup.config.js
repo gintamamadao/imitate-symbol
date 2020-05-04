2effccc571c016dff8e741ae0cc9ceeba65a4082
gintamamadao <gintamaginsan@126.com>
2020-05-04 23:30:33 +0800

    feat::tada:初始化

---
+++ build/rollup.config.js
@@ -0,0 +1,35 @@
+const util = require("./util");
+const typescript = require("rollup-plugin-typescript2");
+const babel = require("rollup-plugin-babel");
+const commonjs = require("rollup-plugin-commonjs");
+
+const extensions = [".js", ".jsx", ".ts", ".tsx"];
+
+const babelOptions = {
+    extensions,
+    runtimeHelpers: true,
+    presets: [
+        [
+            "@babel/env",
+            {
+                modules: false,
+                targets: {
+                    node: "10.15.3",
+                },
+            },
+        ],
+    ],
+};
+
+module.exports = {
+    input: util.resolve("src/index.ts"),
+    output: {
+        file: util.resolve("dist/schema-verify.js"),
+        format: "cjs",
+    },
+    plugins: [
+        typescript(),
+        commonjs({ extensions, ignore: ["conditional-runtime-dependency"] }),
+        babel(babelOptions),
+    ],
+};
